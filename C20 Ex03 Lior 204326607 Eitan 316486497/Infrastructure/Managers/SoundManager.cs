7baeb707d6bd460c2406b364a16a1696e84e6a24
alroy214 <alroy214@gmail.com>
2020-10-04 20:12:25 +0300

    Fixing Music.

---
+++ C20 Ex03 Lior 204326607 Eitan 316486497/Infrastructure/Managers/SoundManager.cs
@@ -0,0 +1,91 @@
+﻿using Infrastructure.ServiceInterfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.ObjectModel;
+
+namespace C20_Ex03_Lior_204326607_Eitan_316486497.SpaceInvaders.Managers
+{
+    public class SoundManager : GameService, ISoundManager
+    {
+        private const bool k_IsMusicOnFromStart = true;
+        private readonly Dictionary<string, SoundEffectInstance> r_SoundsEffects;
+        private readonly Dictionary<string, Song> r_Songs;
+        private bool m_IsGameSoundOn;
+
+        public SoundManager(Game i_Game) : base(i_Game)
+        {
+            m_IsGameSoundOn = k_IsMusicOnFromStart;
+            r_SoundsEffects = new Dictionary<string, SoundEffectInstance>();
+            r_Songs = new Dictionary<string, Song>();
+        }
+
+
+        protected override void RegisterAsService()
+        {
+            this.Game.Services.AddService(typeof(ISoundManager), this);
+        }
+
+        public bool IsGameSoundOn
+        {
+            get { return this.m_IsGameSoundOn; }
+            set { this.m_IsGameSoundOn = value; }
+        }
+
+        public bool AreSoundsOn { get; set; }
+
+        public Dictionary<string, SoundEffectInstance> SoundEffects
+        {
+            get { return this.r_SoundsEffects; }
+        }
+
+        public Dictionary<string, Song> Songs
+        {
+            get { return this.r_Songs; }
+        }
+
+        public void PlaySoundEffect(string i_SoundEffect)
+        {
+            if (this.m_IsGameSoundOn)
+            {
+                //this.r_SoundsEffects[i_SoundEffect].Play();
+            }
+        }
+
+        
+        
+
+        public void AddSoundEffect(SoundEffect i_SoundEffect, string i_SoundName)
+        {
+            this.r_SoundsEffects.Add(i_SoundName, i_SoundEffect.CreateInstance());
+        }
+
+        public void AddSong(Song i_Song, string i_SongName)
+        {
+            this.r_Songs.Add(i_SongName, i_Song);
+        }
+
+        public void RemoveSoundEffect(string i_SoundName)
+        {
+            this.r_SoundsEffects.Remove(i_SoundName);
+        }
+
+        public void RemoveSong(string i_SongName)
+        {
+            this.r_Songs.Remove(i_SongName);
+        }
+
+        public Song GetSong(string i_Name)
+        {
+            return this.r_Songs[i_Name];
+        }
+
+        public SoundEffectInstance GetSoundEffect(string i_Name)
+        {
+            return this.r_SoundsEffects[i_Name];
+        }
+    }
+}
--- C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Invaders.cs
+++ C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Invaders.cs
@@ -2,13 +2,16 @@
 using C20_Ex03_Lior_204326607_Eitan_316486497.GameEntities;
 using C20_Ex03_Lior_204326607_Eitan_316486497.GameEntities.Ships;
 using C20_Ex03_Lior_204326607_Eitan_316486497.Infrastructure;
+using C20_Ex03_Lior_204326607_Eitan_316486497.SpaceInvaders.Managers;
 using C20_Ex03_Lior_204326607_Eitan_316486497.SpaceInvaders.Screens;
 using C20_Ex03_Lior_204326607_Eitan_316486497.SpaceInvaders.Stats;
 using Infrastructure.Managers;
 using Infrastructure.ObjectModel.Screens;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace C20_Ex03_Lior_204326607_Eitan_316486497
 {
@@ -29,6 +32,10 @@ namespace C20_Ex03_Lior_204326607_Eitan_316486497
         private int m_CurrentLivingPlayers;
         public const int k_NumberOfWalls = 4;
         private GameScreen welcomeScreen;
+
+
+        private readonly SoundManager r_SoundManager;
+
         public Invaders() : base()
         {
             r_Graphics.IsFullScreen = false;
@@ -41,6 +48,7 @@ namespace C20_Ex03_Lior_204326607_Eitan_316486497
             
              welcomeScreen = new WelcomeScreen(this);
              r_Screens.SetCurrentScreen(welcomeScreen);
+            r_SoundManager = new SoundManager(this);
         }
 
         protected override void Initialize()
@@ -50,7 +58,22 @@ namespace C20_Ex03_Lior_204326607_Eitan_316486497
 
         protected override void LoadContent()
         {
+            
             m_SpriteBatch = new SpriteBatch(GraphicsDevice);
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"C:/temp/XNA_Assets/Ex03/Sounds/SSGunShot"), "SSGunShot");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/MotherShipKill"), "MotherShipKill");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/MenuMove"), "MenuMove");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/LifeDie"), "LifeDie");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/LevelWin"), "LevelWin");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/GameOver"), "GameOver");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/EnemyKill"), "EnemyKill");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/EnemyGunShot"), "EnemyGunShot");
+            r_SoundManager.AddSoundEffect(Content.Load<SoundEffect>(@"c:/temp/XNA_Assets/Ex03/Sounds/BarrierHit"), "BarrierHit");/*
+            r_SoundManager.AddSong(Content.Load<Song>(@"c:/temp/XNA_Assets/Ex03/Sounds/BGMusic"), "BGMusic");
+            MediaPlayer.Play(r_SoundManager.GetSong("BGMusic"));*/
+            MediaPlayer.IsRepeating = true;
+
+
             base.LoadContent();
         }
 
