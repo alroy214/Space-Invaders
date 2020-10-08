d44cee0638648dd7fb5ad2e7423ae8ad1ba64f26
LiorRey <lior_rey@hotmail.com>
2020-10-08 18:09:19 +0300

    Small change: the line "//using Microsoft.Xna.Framework.Media;" was removed from Invaders.cs

--- C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Invaders.cs
+++ C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Invaders.cs
@@ -11,7 +11,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-//using Microsoft.Xna.Framework.Media;
 
 namespace C20_Ex03_Lior_204326607_Eitan_316486497
 {
