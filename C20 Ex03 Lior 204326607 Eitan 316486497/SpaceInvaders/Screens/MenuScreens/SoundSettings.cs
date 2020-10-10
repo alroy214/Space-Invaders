ef4d14cb9fb5179a238ce46db36da6d78e92d51d
LiorRey <lior_rey@hotmail.com>
2020-10-10 19:47:19 +0300

    Comment remove in SoundSettings.cs line 30: "/*r_SoundManager.SoundToggle*/"

--- C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Screens/MenuScreens/SoundSettings.cs
+++ C20 Ex03 Lior 204326607 Eitan 316486497/SpaceInvaders/Screens/MenuScreens/SoundSettings.cs
@@ -27,7 +27,7 @@ namespace C20_Ex03_Lior_204326607_Eitan_316486497.SpaceInvaders.Screens.MenuScre
             SetScreenHeader(k_HeaderAssetName, k_HeaderScale);
             r_SoundManager = (ISoundManager)i_Game.Services.GetService(typeof(ISoundManager));
             r_InputManager = i_Game.Services.GetService(typeof(IInputManager)) as IInputManager;
-            AddOptionItem(k_ToggleSoundMessage + "Off" /*r_SoundManager.SoundToggle*/, Color.MediumSeaGreen, soundToggle_OnClicked, true);
+            AddOptionItem(k_ToggleSoundMessage + "Off", Color.MediumSeaGreen, soundToggle_OnClicked, true);
             AddOptionItem(k_BackgroundMusicMessage + r_SoundManager.GetBackgroundMusicVolumePercentage(), Color.CornflowerBlue, backgroundMusicMessage_OnClicked, true);
             AddOptionItem(k_SoundEffectsMessage + r_SoundManager.GetSoundEffectsVolumePercentage(), Color.Bisque, soundEffects_OnClicked, true);
             AddOptionItem(k_DoneMessage, Color.PaleVioletRed, done_OnClicked);
